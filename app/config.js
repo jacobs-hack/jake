dd9f92dd6ed6ef443affefc7e2ddd935fe54ab68
nate_getahun <natnael.wubishet.21@gmail.com>
2016-09-12 11:50:17 +0200

    removed sensitive info

--- app/config.js
+++ app/config.js
@@ -12,12 +12,13 @@ module.exports =
     {
         "appSecret": "dslndslnsomethingrandom43423",
         "validationToken": "messenger_jhack_bot_2016",
-        "pageAccessToken": "aabbaaccssdd"
+        "pageAccessToken": "your-page-access-token",
+        "pageId" :"your-page-id"
     },
     // wit keys
     wit: 
     {
-        "appId": "your-wit-id",
-        "serverAccessToken": "your-server-access-token"
+        // "appId": "your-wit-id",
+        "serverAccessToken": "your-wit-token"
     }
 }
