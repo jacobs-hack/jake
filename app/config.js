856d292e7448ea4dc9561e9b2ede17a210deeacc
natnaelwubishet <n.getahun@jacobs-university.com>
2016-09-28 09:12:43 -0400

    Added credentials: DONT PUSH FROM HERE

--- app/config.js
+++ app/config.js
@@ -9,15 +9,14 @@ module.exports =
     // messenger keys
     messenger:
     {
-        "appSecret": "your-app-secret",
-        "validationToken": "your-validation-token",
-        "pageAccessToken": "your-page-access-token",
-        "pageId" :"your-page-id"
+        "appSecret": "ff895c017dfd60576b0809b62d43514d",
+        "validationToken": "jake_validation_token",
+        "pageAccessToken": "EAAJCSPlt9rUBAIVWAmmlBhY5qOn89YjhEQSVEPprvoHZA1PmZC3WFLLpF8oNUXnawJGbQr6cLYg7GDRsQfJwZCLDXBtFokXvamnMo1j8HFxsBe7dl7mEmvZBrnZAuJrbnFMT6qb8xiDBk3vSZCJYsBTKg7qt7CE6ZBbQ6PYfqIunQZDZD",
+        "pageId" :"513182455491932"
     },
     // wit keys
     wit:
     {
-        // "appId": "your-wit-id",
-        "serverAccessToken": "your-wit-token"
+        "serverAccessToken": "Q3WJD7IUBGLV2V7XA5IWAAWZBMUZCIXM"
     }
 }
